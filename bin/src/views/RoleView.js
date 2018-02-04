db555d92050372b72277e4efe0c94fa2f83a1c3d
Tai Nguyen <nguyentai.fit@gmail.com>
2018-02-04 14:33:33 +0700

    add missing file

---
+++ bin/src/views/RoleView.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var RoleType;
+(function (RoleType) {
+    RoleType[RoleType["Admin"] = 1] = "Admin";
+    RoleType[RoleType["Lender"] = 2] = "Lender";
+    RoleType[RoleType["Borrower"] = 3] = "Borrower";
+})(RoleType = exports.RoleType || (exports.RoleType = {}));
